068fd65123e3ee12a887026ade68ff90854b7bb8
Sophia Liu <sl3632@cornell.edu>
2026-09-15 23:19:47 -0400

    Fix: Turn off bot protection
    Problem: CMV Reddit mod wanted to copy and paste, but the experiment didn't allow it because it automatically had bot protection on (bot protection = disallow copy and pasting in chat input)
    Solution: Set the botProtection to false
    Testing: Tested locally with assistant branch

--- app/api/create-experiment/generator.ts
+++ app/api/create-experiment/generator.ts
@@ -246,7 +246,7 @@ export async function generate(p1: string, p2: string, experimentTemplatePath: s
             minParticipantsPerCohort: cfg.minParticipantsPerCohort ?? 2,
             maxParticipantsPerCohort: cfg.maxParticipantsPerCohort ?? 2,
             includeAllParticipantsInCohortCount: cfg.includeAllParticipantsInCohortCount ?? true,
-            botProtection: cfg.botProtection ?? true,
+            botProtection: cfg.botProtection ?? false,
           },
         })),
       }),
